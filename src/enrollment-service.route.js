d19937ff6203098199a3b4cae34120120da6c8b1
Vignesh Suryah Mani <vigneshsuryah.blue@gmail.com>
2019-09-17 16:52:44 +0530

    Initial Commit

---
+++ src/enrollment-service.route.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var in_memory_db_1 = require("./in-memory-db");
+function saveEnrollment(req, res) {
+    var enrollment = req.body;
+    console.log('Received the Enrollment: ');
+    console.log(enrollment);
+    in_memory_db_1.ENROLLMENTS.push(enrollment);
+    res.status(200).json({ message: "Enrollment added successfully." });
+}
+exports.saveEnrollment = saveEnrollment;
+function getAllEnrollments(req, res) {
+    res.status(200).json(in_memory_db_1.ENROLLMENTS);
+}
+exports.getAllEnrollments = getAllEnrollments;
+function getAllStates(req, res) {
+    res.status(200).json({"states":['CA','CO','WI','VA','OH','GA','IN','KY','NY','NV','NH']});
+}
+exports.getAllStates = getAllStates;
+function getAllPlans(req, res) {
+    res.status(200).json({"states":['Anthem Senior Advantage Basic - HMO','Anthem Senior Advantage Plus - HMO','Anthem Balanced Blue Choice Basic - PPO','Anthem Balanced Blue Choice Plus - PPO','Blue Cross Senior Secure Basic - HMO','Blue Cross Senior Secure Plus - HMO']});
+}
+exports.getAllPlans = getAllPlans;
+function clearAllEnrollments(req, res) {
+    in_memory_db_1.ENROLLMENTS = [];
+    res.status(200).json(in_memory_db_1.ENROLLMENTS);
+}
+exports.clearAllEnrollments = clearAllEnrollments;
